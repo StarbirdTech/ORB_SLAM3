0770861e0a1f7270a0f81563cf8de33627afc1f9
StarbirdTech <64431622+StarbirdTech@users.noreply.github.com>
2024-04-21 23:08:58 -0400

    Add verbosity option to CMakeLists and build.sh

--- build.sh
+++ build.sh
@@ -1,54 +1,34 @@
+#!/bin/bash
+
+# Default verbosity flag
+VERBOSE_FLAG=""
+
+# Parse command line arguments
+for arg in "$@"
+do
+    case $arg in
+        -v|--verbose)
+        VERBOSE_FLAG="-DVERBOSE=ON"
+        shift # Remove --verbose or -v from processing
+        ;;
+    esac
+done
+
 echo "Configuring and building Thirdparty/DBoW2 ..."
-
-# cd Thirdparty/DBoW2
-# mkdir build
-# cd build
-# cmake .. -DCMAKE_BUILD_TYPE=Release
-# make -j
-
 cmake -S Thirdparty/DBoW2 -B Thirdparty/DBoW2/build
 cmake --build Thirdparty/DBoW2/build
 
-# cd ../../g2o
-
 echo "Configuring and building Thirdparty/g2o ..."
-
-# mkdir build
-# cd build
-# cmake .. -DCMAKE_BUILD_TYPE=Release
-# make -j
-
 cmake -S Thirdparty/g2o -B Thirdparty/g2o/build
 cmake --build Thirdparty/g2o/build
 
-# cd ../../Sophus
-
 echo "Configuring and building Thirdparty/Sophus ..."
-
-# mkdir build
-# cd build
-# cmake .. -DCMAKE_BUILD_TYPE=Release
-# make -j
-
 cmake -S Thirdparty/Sophus -B Thirdparty/Sophus/build
 cmake --build Thirdparty/Sophus/build
 
-# cd ../../../
-
 echo "Uncompress vocabulary ..."
-
-# cd Vocabulary
-# tar -xf ORBvoc.txt.tar.gz
-# cd ..
-
 tar -xzf Vocabulary/ORBvoc.txt.tar.gz -C Vocabulary
 
 echo "Configuring and building ORB_SLAM3 ..."
-
-# mkdir build
-# cd build
-# cmake .. -DCMAKE_BUILD_TYPE=Release
-# make -j4
-
-cmake -S . -B build
+cmake -S . -B build $VERBOSE_FLAG
 cmake --build build
